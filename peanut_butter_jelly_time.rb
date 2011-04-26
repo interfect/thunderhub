af014d4b637f1a5361d690ca4ee5ccf771eb1a44
Miloš Hadžić <milos.hadzic@gmail.com>
2011-04-26 09:12:52 +0200

    Peanut butter jelly time

---
+++ peanut_butter_jelly_time.rb
@@ -0,0 +1,2 @@
+while true do print "pea";sleep 0.3;print "nut";sleep 0.3;print " bu";sleep 0.2;print "tter";sleep 0.3;print " je";sleep 0.2;print "lly ";sleep 0.3;print "ti";sleep 0.1;print "me!";sleep 0.2;print "\n";sleep 0.2;end
+
